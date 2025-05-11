e6ce55cb421b20aa798a7a4d234d30b2e6ef10e1
nico santamaria <nico.santamaria@whistl.co.uk>
2025-05-11 18:43:07 +0100

    refactoring

---
+++ include/zs_esp_32_sd_card.h
@@ -0,0 +1,22 @@
+#ifndef __ZS_ESP32_SD_CARD__
+#define __ZS_ESP32_SD_CARD__
+#include "FS.h"
+#include "SD_MMC.h"
+#include <zs_serial_port_utils.h>
+
+#define PIN_SPI_CS 5 // The ESP32 pin GPIO5
+
+namespace ZS
+{
+   namespace Esp32
+   {
+      namespace SdCard
+      {
+         bool initializeSdCard();
+         double getSdCardSizeInGB();
+         String getSdCardType();
+      }
+   }
+}
+
+#endif
--- src/zs_esp_32_sd_card.h
+++ src/zs_esp_32_sd_card.h
@@ -2,7 +2,8 @@
 #define __ZS_ESP32_SD_CARD__
 #include "FS.h"
 #include "SD_MMC.h"
-#include "zs_serial_port_utils.h"
+#include <zs_serial_port_utils.h>
+#include <zs_esp_32_sd_card.h>
 
 #define PIN_SPI_CS 5 // The ESP32 pin GPIO5
 
